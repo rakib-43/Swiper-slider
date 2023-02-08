f436e0fb43506a93dd26bb580b71f051335bf430
Rakib hasan <hasanrakib739@gmail.com>
2023-02-08 21:02:09 +0600

    swiper slider for member

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+//<!-- Initialize Swiper -->
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 35,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
